416348ab71673ea3ec8b4f9939bcfc13b59c8245
Naveen Naik <naveen.naik@bitloka.com>
2023-05-24 11:58:04 +0530

    first commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       // implement node event listeners here
     },
   },
-})
+  experimentalWebKitSupport: true,
+  video:false
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,122 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+require('cy-verify-downloads').addCustomCommand();
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/admin"]').click()
+  })
+})
+Cypress.Commands.add('accdamic_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/academics"]').click()
+  })
+})
+Cypress.Commands.add('connect_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/connect"]').click()
+  })
+})
+Cypress.Commands.add('vault_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/vault"]').click()
+  })
+})
+Cypress.Commands.add('fee_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/fees"]').click()
+  })
+})
+Cypress.Commands.add('library_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/library"]').click()
+  })
+})
+  Cypress.Commands.add('receipt_login', (username, password) => {
+    cy.session([username, password], () => {
+      cy.visit('/')
+      cy.get('input[placeholder="Email"]').type(username)
+      cy.get('#password').type(password)
+      cy.get('input[type="submit"]').click()
+      cy.get('a[href="/library"]').click()
+    })
+})
+Cypress.Commands.add('assets_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/assets"]').click()
+  })
+})
+Cypress.Commands.add('lms_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/lms"]').click()
+  })
+})
+Cypress.Commands.add('admission_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/admissions"]').click()
+  })
+})
+Cypress.Commands.add('tt_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/ttPlanner"]').click()
+  })
+})
+Cypress.Commands.add('hr_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/hr"]').click()
+  })
+})
+Cypress.Commands.add('rc_login', (username, password) => {
+  cy.session([username, password], () => {
+    cy.visit('/')
+    cy.get('input[placeholder="Email"]').type(username)
+    cy.get('#password').type(password)
+    cy.get('input[type="submit"]').click()
+    cy.get('a[href="/regulatoryCompliance"]').click()
+  })
+})
